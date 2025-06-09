53599687c70dcceb891dafb6ec114c76b15c379d
Afri <112723719+afriwondimu@users.noreply.github.com>
2025-06-09 22:09:05 +0300

    Second Commit

--- src/iucpark/manager/RecordManager.java
+++ src/iucpark/manager/RecordManager.java
@@ -1,7 +1,7 @@
 package iucpark.manager; // Defines package
 
-public interface RecordManager { // Defines parking management methods
-    void checkIn(int couponCode, int plateNumber); // Records vehicle entry
-    void checkOut(int couponCode); // Records vehicle exit
-    void exportRecords(String date, Integer plateNumber); // Exports records
+public interface RecordManager { 
+    void checkIn(int couponCode, int plateNumber); 
+    void checkOut(int couponCode); 
+    void exportRecords(String date, Integer plateNumber); 
 }
