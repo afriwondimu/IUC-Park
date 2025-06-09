b764d8347e239cf824161f1ee47406ecf26966d9
Afri <112723719+afriwondimu@users.noreply.github.com>
2025-06-09 22:13:30 +0300

    Second Commit

--- src/iucpark/model/Vehicle.java
+++ src/iucpark/model/Vehicle.java
@@ -4,14 +4,14 @@ import java.io.Serializable; // Allows serialization
 import java.time.LocalDateTime; // Represents date-time
 
 public abstract class Vehicle implements Serializable { // Blueprint for vehicles
-    protected int couponCode; // Stores coupon code
-    protected int plateNumber; // Stores plate number
-    protected LocalDateTime checkInTime; // Stores check-in time
+    protected int couponCode; 
+    protected int plateNumber; 
+    protected LocalDateTime checkInTime; 
 
     public Vehicle(int couponCode, int plateNumber, LocalDateTime checkInTime) { // Constructor
-        this.couponCode = couponCode; // Sets coupon code
-        this.plateNumber = plateNumber; // Sets plate number
-        this.checkInTime = checkInTime; // Sets check-in time
+        this.couponCode = couponCode; 
+        this.plateNumber = plateNumber; 
+        this.checkInTime = checkInTime; 
     }
 
     public int getCouponCode() { return couponCode; } // Returns coupon code
