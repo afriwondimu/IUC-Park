b00e664dbb056dc14815b556dffc42c6c1f4431e
Afri <112723719+afriwondimu@users.noreply.github.com>
2025-06-09 22:26:53 +0300

    Second Commit

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import iucpark.manager.Iuc_Park_Main; // Imports main class
 
 public class App { // Main program class
     public static void main(String[] args) { // Program entry point
-        Iuc_Park_Main system = new Iuc_Park_Main(); // Creates parking system
-        system.run(); // Runs system
+        Iuc_Park_Main system = new Iuc_Park_Main(); // Creating obj
+        system.run(); 
     }
 }
